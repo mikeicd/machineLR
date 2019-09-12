80b59cd1eb187d1f383f9d14f3c8bc3de3b7b807
Mike <mikeicd@gmail.com>
2019-09-11 21:33:06 -0300

    Aula de quarta

---
+++ spam.r
@@ -0,0 +1,3 @@
+sms_raw <- read.csv("sms_spam.csv", stringsAsFactors = FALSE)
+
+str(sms_raw)
---
+++ teens.r
@@ -0,0 +1,35 @@
+teens <- read.csv("snsdata.csv")
+
+str(teens)
+
+#summary(teens$age)
+
+teens$age <- ifelse(teens$age >= 13 & teens$age < 20, teens$age, NA)
+
+#summary(teens$age)
+
+#aqui só foi criado como se fosse uma tag, elas dependen do gender
+teens$female <- ifelse(teens$gender == "F" & !is.na(teens$gender), 1, 0)
+teens$no_gender <- ifelse(is.na(teens$gender), 1, 0)
+
+#table(teens$gender, useNA = "ifany")
+#table(teens$female, useNA = "ifany")
+#table(teens$no_gender, useNA = "ifany")
+
+ave_age <- ave(teens$age, teens$gradyear, FUN = function(x) mean(x, na.rm = TRUE))
+
+teens$age <- ifelse(is.na(teens$age), ave_age, teens$age)
+
+interests <- teens[5:40]
+interests_z <- as.data.frame(lapply(interests, scale))
+
+set.seed(2345)
+teen_clusters <- kmeans(interests_z, 5)
+
+teen_clusters$size
+
+teen_clusters$centers
+
+teens$cluster <- teen_clusters$cluster
+
+teens[1:5, c("cluster", "gender", "age", "friends")]
